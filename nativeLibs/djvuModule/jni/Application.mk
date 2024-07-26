e64ef58d11ad764e102605ca2c25394dd9ee1674
Numan Ali <flopy6663@gmail.com>
2024-07-26 13:24:14 +0500

    Removing no needing modules

--- nativeLibs/djvuModule/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-# The ARMv7 is significanly faster due to the use of the hardware FPU
-#armeabi mips
-APP_ABI := armeabi-v7a x86
-APP_PLATFORM=android-16
-APP_OPTIM := release
-APP_STL := system
-APP_CPPFLAGS += -fexceptions
-APP_STL := c++_static
